9a9b3791d5b62a56b4a0147eb092dcc7d8441d8d
Muench-Shi <3311386889@qq.com>
2021-12-04 22:56:48 +0800

    update

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 mkdir -p ~/.streamlit/
 
 echo "\
+[general]\n\
+email = \"muenchshi@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+
+echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
