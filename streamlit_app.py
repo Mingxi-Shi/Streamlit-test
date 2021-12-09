8ab163ea3e0eccefa237fc7d6c8c0ea9f1c7b162
Muench-Shi <3311386889@qq.com>
2021-12-09 11:10:24 +0800

    upload vedio script

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
 st.write("asdsada")
+
+vid_file1 = open("./vedio/7.mp4", "rb").read()
+st.video(vid_file1)
+
+vid_file2 = open("./vedio/star.mp4", "rb").read()
+st.video(vid_file2)
