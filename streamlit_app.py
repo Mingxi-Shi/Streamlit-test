8cb10fb3861bd57bb4e3451c63afc9ba5b687a64
Muench-Shi <3311386889@qq.com>
2021-12-09 11:26:55 +0800

    modify script

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,8 @@ st.video(vid_file1)
 vid_file2 = open("./vedio/star.mp4", "rb").read()
 st.video(vid_file2)
 
-audio_file1 = open("稻香-周杰伦.mp3", "rb").read()
+audio_file1 = open("./music/稻香-周杰伦.mp3", "rb").read()
 st.audio(audio_file1, format='audio/mp3', start_time=0)
 
-audio_file2 = open("七里香-周杰伦.mp3", "rb").read()
+audio_file2 = open("./music/七里香-周杰伦.mp3", "rb").read()
 st.audio(audio_file2, format='audio/mp3', start_time=0)
