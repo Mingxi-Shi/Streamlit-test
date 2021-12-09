dd32cb14201a2deb79f67ab44b88f0069726a765
Muench-Shi <3311386889@qq.com>
2021-12-09 11:21:28 +0800

    upload music

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,9 @@ st.video(vid_file1)
 
 vid_file2 = open("./vedio/star.mp4", "rb").read()
 st.video(vid_file2)
+
+audio_file1 = open("稻香-周杰伦.mp3", "rb").read()
+st.audio(audio_file1, format='audio/mp3', start_time=0)
+
+audio_file2 = open("七里香-周杰伦.mp3", "rb").read()
+st.audio(audio_file2, format='audio/mp3', start_time=0)
