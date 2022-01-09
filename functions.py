daf755f07b2f3989d91bb9e94a68dcb75690cb66
Muench-Shi <3311386889@qq.com>
2022-01-09 22:54:26 +0800

    2022-1-9-14

--- functions.py
+++ functions.py
@@ -6,8 +6,6 @@ import pandas as pd
 def page1():
     data = st.file_uploader("上传数据", type=["csv", 'txt', 'xlsx', 'xls'])
     if data is not None:
-        st.write(data.name)
-        st.write(data.type)
         if data.name[-3:] == "csv" or data.name[-3:] == "txt":
             df = pd.read_csv(data)
             st.dataframe(df)
@@ -28,6 +26,27 @@ def page1():
                            help='click to download the above data as XLSX(one sheet)'
                            # https://discuss.streamlit.io/t/download-xlsx-file-with-multiple-sheets-in-streamlit/11509/2
                            )
+        st.write("每个功能模块都在container内")
+        # 功能0：修改列数据类型
+        with st.expander(label="功能0：修改数据类型", expanded=False):
+            with st.container():
+                df_columns_name = df.columns.to_list()
+                modified_datatype = [' ' for _ in range(len(df.columns))]
+                p1, p2 = st.columns([1, 5])
+                with p1:
+                    for i in range(len(df_columns_name)):
+                        modified_datatype[i] = st.selectbox(label='选择列 [ ' + df_columns_name[i] + ' ]的数据类型',
+                                                            options=['string', 'int64', 'float64', 'bool'], key=i,
+                                                            index=judge_original_datatype(df, i))
+                    temp = convert_df_columns_datatype(df, df_columns_name, modified_datatype)
+                    if st.button("确认修改", key='modify_datatype'):
+                        st.write('success')
+                        df = temp
+
+                with p2:
+                    st.text('')
+                    st.text('')
+                    st.write(df.head(20))
 
 
 def page2():
@@ -88,3 +107,29 @@ def convert2excel_df(df):
     writer.save()
     processed_data = output.getvalue()
     return processed_data
+
+
+def judge_original_datatype(df, i):
+    index = 0
+    df_columns_name = df.columns.to_list()
+    if df[df_columns_name[i]].dtype == 'string':
+        index = 0
+    elif df[df_columns_name[i]].dtype == 'int64':
+        index = 1
+    elif df[df_columns_name[i]].dtype == 'float64':
+        index = 2
+    elif df[df_columns_name[i]].dtype == 'bool':
+        index = 3
+    return index
+
+
+def convert_df_columns_datatype(df, cols, types):
+    df_columns_name = df.columns.to_list()
+    for i in range(len(cols)):
+        if df[cols[i]].dtype == 'object':
+            if types[i] == 'int64' or types == 'float64':
+                st.write('列 [ ', cols[i], ' ] 不能修改为', types[i], '型')
+                df[cols[i]] = df[cols[i]].astype('string')
+        else:
+            df[cols[i]] = df[cols[i]].astype(types[i])
+    return df
