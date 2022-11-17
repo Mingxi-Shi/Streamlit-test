335260d056065c8feed24d2ee0f8aa035a422e9b
Mingxi-Shi <3311386889@qq.com>
2022-11-17 11:34:12 +0800

    11-17-3

--- pages/_🌏_test_1.py
+++ pages/_🌏_test_1.py
@@ -11,15 +11,16 @@ def main():
         '''
     st.markdown(sysmenu, unsafe_allow_html=True)
 
-    ip_str = st.text_area(label='输入ping的IP:', height=400)
-    ip_list = [i for i in ip_str.split()]
-    if st.button("开始ping"):
-        for i in range(len(ip_list)):
-            result = str(ping(ip_list[i], verbose=True))
-            if result.count('Reply') == 4:
-                st.write(ip_list[i], "  通")
-            elif result.count('timed out') == 4:
-                st.write(ip_list[i], "  不通")
+    # ip_str = st.text_area(label='输入ping的IP:', height=400)
+    # ip_list = [i for i in ip_str.split()]
+    # if st.button("开始ping"):
+    #     for i in range(len(ip_list)):
+    #         result = str(ping(ip_list[i], verbose=True))
+    #         if result.count('Reply') == 4:
+    #             st.write(ip_list[i], "  通")
+    #         elif result.count('timed out') == 4:
+    #             st.write(ip_list[i], "  不通")
+    st.write(1)
 
 
 if __name__ == '__main__':
