45a5b51096ca51b7f135d0055cb2255f4db412db
Mingxi-Shi <3311386889@qq.com>
2022-11-24 11:07:23 +0800

    11-24-1

--- pages/_🌸_image_process.py
+++ pages/_🌸_image_process.py
@@ -1,4 +1,5 @@
 # Done by Muench
+# Done by Muench
 import base64
 import io
 
@@ -7,11 +8,30 @@ import cv2
 import numpy as np
 from PIL import Image, ImageEnhance
 from aip import AipImageProcess
+from streamlit_cropper import st_cropper
 
 
-def get_file_content(filePath):
-    with open(filePath, 'rb') as fp:
-        return fp.read()
+def cartonize_image(original_image):
+    rgb_array_img = np.array(original_image.convert('RGB'))
+    img = cv2.cvtColor(rgb_array_img, 1)
+    gray = cv2.cvtColor(rgb_array_img, cv2.COLOR_BGR2GRAY)
+    # Edges
+    gray = cv2.medianBlur(gray, 5)
+    edges = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 9)
+    # Color
+    color = cv2.bilateralFilter(img, 9, 300, 300)
+    # Cartoon
+    cartoon = cv2.bitwise_and(color, color, mask=edges)
+    return cartoon
+
+
+def cannize_image(original_image):
+    rgb_array_img = np.array(original_image.convert('RGB'))
+    img = cv2.cvtColor(rgb_array_img, 1)
+    img = cv2.GaussianBlur(img, (11, 11), 0)
+    canny = cv2.Canny(img, 100, 150)
+    return canny
+
 
 def main():
     st.set_page_config(page_title="图片处理", page_icon="🌸", layout="wide")
@@ -31,23 +51,56 @@ def main():
 
     client = AipImageProcess(APP_ID, API_KEY, SECRET_KEY)
 
-    selected_model = st.sidebar.selectbox(label=" ", options=['图像特效', '图像增强'], label_visibility="collapsed")
-    if selected_model == "图像特效":
-        selected_func_1 = st.sidebar.selectbox(label=" ", options=["黑白图像上色", "图像风格转换", "人像动漫化"], label_visibility="collapsed")
+    img_file = st.sidebar.file_uploader(label='上传图片', type=['png', 'jpg'])
 
-        img = st.sidebar.file_uploader(label="上传图片", type=['jpg', 'png'], key="txtx")
-        if img:
+    selected_model = st.sidebar.selectbox(label="选择模块", options=['图像裁剪', '图像特效', '图像增强'], label_visibility="collapsed")
+
+    if selected_model == "图像裁剪":
+        realtime_update = st.sidebar.checkbox(label="Update in Real Time", value=True)
+        aspect_choice = st.sidebar.radio(label="纵横比", options=["1:1", "16:9", "4:3", "2:3", "自定义"])
+        aspect_dict = {
+            "1:1": (1, 1),
+            "16:9": (16, 9),
+            "4:3": (4, 3),
+            "2:3": (2, 3),
+            "自定义": None
+        }
+        aspect_ratio = aspect_dict[aspect_choice]
+
+        if img_file:
+            img = Image.open(img_file)
+
+            if not realtime_update:
+                st.write("Double click to save crop")
+            # Get a cropped image from the frontend
+            cropped_img = st_cropper(
+                img,
+                realtime_update=realtime_update,
+                aspect_ratio=aspect_ratio,
+                return_type='image'
+            )
+
+            # Manipulate cropped image at will
+            st.write("Preview")
+            _ = cropped_img.thumbnail((150, 150))
+            st.image(cropped_img)
+
+    elif selected_model == "图像特效":
+        func_list_1 = ["黑白图像上色", "图像风格转换", "人像动漫化", "卡通化", "黑白简体化"]
+        selected_func_1 = st.sidebar.selectbox(label="选择功能", options=func_list_1, label_visibility="collapsed")
+
+        if img_file:
             if selected_func_1 == "黑白图像上色":
-                res_image = client.colourize(image=img.getvalue())
+                res_image = client.colourize(image=img_file.getvalue())
                 st.image(base64.b64decode(res_image['image']))
 
             elif selected_func_1 == "图像风格转换":
                 options_1 = {}
                 style_list = ["cartoon", "pencil", "color_pencil", "warm", "wave", "lavender", "mononoke", "scream",
                               "gothic"]
-                selected_option = st.sidebar.selectbox(label="", options=style_list, label_visibility="collapsed")
+                selected_option = st.sidebar.selectbox(label="选择风格", options=style_list, label_visibility="collapsed")
                 options_1['option'] = selected_option
-                res_image = client.styleTrans(image=img.getvalue(), options=options_1)
+                res_image = client.styleTrans(image=img_file.getvalue(), options=options_1)
                 st.image(base64.b64decode(res_image['image']))
 
             elif selected_func_1 == "人像动漫化":
@@ -56,29 +109,71 @@ def main():
                 if whether_mask:
                     options_2['type'] = "anime_mask"
                     options_2['mask_id'] = st.sidebar.slider(label='口罩类型', min_value=1, max_value=8)
-                res_image = client.selfieAnime(image=img.getvalue(), options=options_2)
+                res_image = client.selfieAnime(image=img_file.getvalue(), options=options_2)
                 st.image(base64.b64decode(res_image['image']))
 
-    elif selected_model == "图像增强":
-        func_list = ["图像去雾", "图像对比度增强", "图像无损放大", "拉伸图像恢复", "图像修复", "图像清晰度增强", "天空分割"]
-        selected_func_2 = st.sidebar.selectbox(label=" ", options=func_list, label_visibility="collapsed")
+            elif selected_func_1 == "卡通化":
+                original_image = Image.open(img_file)
+                res_image = cartonize_image(original_image)
+                st.image(res_image)
 
-        img = st.sidebar.file_uploader(label="上传图片", type=['jpg', 'png'], key="txzq")
-        if img:
-            if selected_func_2 == "图像去雾":
-                res_image = client.dehaze(image=img.getvalue())
+            elif selected_func_1 == "黑白简体化":
+                original_image = Image.open(img_file)
+                res_image = cannize_image(original_image)
+                st.image(res_image)
+
+    elif selected_model == "图像增强":
+        func_list_2 = ["灰度", "对比度", "亮度", "模糊度", "图像去雾", "图像对比度增强", "图像无损放大", "拉伸图像恢复", "图像修复", "图像清晰度增强", "天空分割"]
+        selected_func_2 = st.sidebar.selectbox(label="选择功能", options=func_list_2, label_visibility="collapsed")
+
+        if img_file:
+            if selected_func_2 == "灰度":
+                original_image = Image.open(img_file)
+                rgb_array_img = np.array(original_image.convert('RGB'))
+                img = cv2.cvtColor(rgb_array_img, 1)
+                gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                st.text("处理灰度后:")
+                st.image(gray_img)
+
+            elif selected_func_2 == "对比度":
+                original_image = Image.open(img_file)
+                c_rate = st.sidebar.slider("Contrast", 0.5, 3.5)
+                enhancer = ImageEnhance.Contrast(original_image)
+                contrast_img = enhancer.enhance(c_rate)
+                st.text("处理对比度后:")
+                st.image(contrast_img)
+
+            elif selected_func_2 == "亮度":
+                original_image = Image.open(img_file)
+                c_rate = st.sidebar.slider("Brightness", 0.5, 3.5)
+                enhancer = ImageEnhance.Brightness(original_image)
+                brightness_img = enhancer.enhance(c_rate)
+                st.text("处理亮度后:")
+                st.image(brightness_img)
+
+            elif selected_func_2 == "模糊度":
+                original_image = Image.open(img_file)
+                rgb_array_img = np.array(original_image.convert('RGB'))
+                blur_rate = st.sidebar.slider("Brightness", 0.5, 3.5)
+                img = cv2.cvtColor(rgb_array_img, 1)
+                blur_img = cv2.GaussianBlur(img, (11, 11), blur_rate)
+                st.text("处理模糊度后:")
+                st.image(blur_img)
+
+            elif selected_func_2 == "图像去雾":
+                res_image = client.dehaze(image=img_file.getvalue())
                 st.image(base64.b64decode(res_image['image']))
 
             elif selected_func_2 == "图像对比度增强":
-                res_image = client.contrastEnhance(image=img.getvalue())
+                res_image = client.contrastEnhance(image=img_file.getvalue())
                 st.image(base64.b64decode(res_image['image']))
 
             elif selected_func_2 == "图像无损放大":
-                res_image = client.imageQualityEnhance(image=img.getvalue())
+                res_image = client.imageQualityEnhance(image=img_file.getvalue())
                 st.image(base64.b64decode(res_image['image']))
 
             elif selected_func_2 == "拉伸图像恢复":
-                res_image = client.stretchRestore(image=img.getvalue())
+                res_image = client.stretchRestore(image=img_file.getvalue())
                 st.image(base64.b64decode(res_image['image']))
 
             elif selected_func_2 == "图像修复":
@@ -89,11 +184,11 @@ def main():
                 st.write(1)
 
             elif selected_func_2 == "图像清晰度增强":
-                res_image = client.imageDefinitionEnhance(image=img.getvalue())
+                res_image = client.imageDefinitionEnhance(image=img_file.getvalue())
                 st.image(base64.b64decode(res_image['image']))
 
             elif selected_func_2 == "天空分割":
-                res_image = client.skySeg(image=img.getvalue())
+                res_image = client.skySeg(image=img_file.getvalue())
                 st.image(base64.b64decode(res_image['image']))
 
 
