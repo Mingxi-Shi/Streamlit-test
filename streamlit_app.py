910fef692d4cd52267c01692735d4829d477158b
Mingxi-Shi <3311386889@qq.com>
2022-11-25 09:39:50 +0800

    Signed-off-by: Mingxi-Shi <3311386889@qq.com>

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,21 @@
 import streamlit as st
+
+
 def main():
-    st.write(1)
+    st.set_page_config(page_title="Muench\'s Tools Page",
+                       page_icon="💎",
+                       layout="wide",
+                       initial_sidebar_state="auto",
+                       menu_items={'About': None}
+                       )
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+
+    hide_menu_style = """
+                    <style>
+                    #MainMenu {visibility: hidden;}
+                    </style>
+                    """
+    st.markdown(hide_menu_style, unsafe_allow_html=True)  # 隐藏网页右上角的菜单
 
 
 if __name__ == '__main__':
